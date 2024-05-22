fff83a6b125d43d32551c589ef0177f0b32c4987
Stomper90845 <168742587+Stomper90845@users.noreply.github.com>
2024-05-22 08:22:53 +0000

    dasda

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,8 +3,14 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
+        
+        cat1 = new Cat();
+        //System.out.println(new Cat());
+        //System.out.println(new Cat());
+        
 
 
-    System.out.println("Fuck the shit i am out!");}
+
+    }
 }
 
